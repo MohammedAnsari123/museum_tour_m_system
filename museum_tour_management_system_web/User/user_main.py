fc118b616315c78d7b0647695dc7fcbec8121fa9
MohammedAnsari123 <ansarimohammed213161@gmail.com>
2025-07-14 21:37:46 +0530

    museum

--- museum_tour_management_system_web/User/user_main.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-
-sidsaction = st.sidebar.radio("", options = ["Home", "Dashboard", "About Us", "Contact Us", "Login", "Sign Up"])
-
-
--- museum_tour_management_system_web/app.py
+++ museum_tour_management_system_web/app.py
@@ -2,17 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# @app.route("/")
+# def first_page():
+#     return "<p>This is first page</p>"
+
 @app.route("/")
 def user_login():
     return render_template("user_login.html")
+    # return "<p>This is user login page</p>"
 
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
-
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
+@app.route("/")
+def admin_login():
+    return render_template("admin_login.html")
+    # return "<p>This is admin login page</p>"
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
+
